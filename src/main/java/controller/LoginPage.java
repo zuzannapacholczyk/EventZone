bcc58c292cd236fb327905dd2ae9b1b0c83adea4
malecbar <bartosz.malecki@brightone.pl>
2015-04-16 16:29:17 +0200

    minor css change

--- src/main/java/controller/LoginPage.java
+++ src/main/java/controller/LoginPage.java
@@ -10,17 +10,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 @SessionAttributes
 public class LoginPage {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView getMainPage() {
-
-		ModelAndView model = new ModelAndView("index");
-
-		return model;
-	}
+	/*
+	 * @RequestMapping(method = RequestMethod.GET) public ModelAndView getMainPage() {
+	 *
+	 * ModelAndView model = new ModelAndView("index");
+	 *
+	 * return model; }
+	 */
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String printWelcome(final ModelMap model, final Principal principal) {
