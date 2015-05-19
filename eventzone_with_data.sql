516b9329dd540d58fe538daaa9a2995ea2889eca
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2015-05-19 11:34:20 +0200

    sql tables changed

--- eventzone_with_data.sql
+++ eventzone_with_data.sql
@@ -10,9 +10,9 @@ Insert into EVENTZONE.USERS (LOGIN,PASSWORD,NAME,SURNAME,EMAIL) values ('zu','zu
 Insert into EVENTZONE.USERS (LOGIN,PASSWORD,NAME,SURNAME,EMAIL) values ('zuss','zuss','zuss','zuss','zuss');
 REM INSERTING into EVENTZONE.EVENTS
 SET DEFINE OFF;
-Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('1','sting','zuzanna','wroclaw',to_date('15/09/11','RR/MM/DD'),to_date('15/04/08','RR/MM/DD'),'it will be awesome');
-Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('2','juwenalia','bartek','krakow',to_date('15/05/14','RR/MM/DD'),to_date('15/04/23','RR/MM/DD'),'have fun');
-Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('3','festiwal','user','warsaw',to_date('15/04/10','RR/MM/DD'),to_date('15/04/01','RR/MM/DD'),'sss');
+Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('1','sting','zuzanna','wroclaw',to_date('15/09/11','RR/MM/DD'),to_date('15/04/08','RR/MM/DD'),'it will be awesome', null);
+Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('2','juwenalia','bartek','krakow',to_date('15/05/14','RR/MM/DD'),to_date('15/04/23','RR/MM/DD'),'have fun', null);
+Insert into EVENTZONE.EVENTS (ID,NAME,ORGANIZER,PLACE,DATEOFEVENT,DATEOFCREATION,DESCRIPTION) values ('3','festiwal','user','warsaw',to_date('15/04/10','RR/MM/DD'),to_date('15/04/01','RR/MM/DD'),'sss', null);
 REM INSERTING into EVENTZONE.TICKETS
 SET DEFINE OFF;
 Insert into EVENTZONE.TICKETS (EVENTID,QUANTITY,COST,ID) values ('1','20','150','1150');
